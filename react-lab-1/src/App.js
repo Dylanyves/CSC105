dbf5af353572cc1760fb151545c2cc82f7f71efd
Dylan Mac Yves <macyvesdylan@gmail.com>
2023-03-30 11:57:57 +0700

    routing

--- react-lab-1/src/App.js
+++ react-lab-1/src/App.js
@@ -1,6 +1,18 @@
 import "./App.css";
 import TodoPage from "./pages/TodoPage";
+import TodayPage from "./pages/TodayPage";
+import Calendar from "./pages/Calendar";
+import HomePage from "./pages/HomePage";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-    return <TodoPage />;
+    return (
+        <Routes>
+            <Route exect path="/" element={<HomePage />} />
+            <Route exect path="/todo" element={<TodoPage />} />
+            <Route exect path="/today" element={<TodayPage />} />
+            <Route exect path="/calendar" element={<Calendar />} />
+        </Routes>
+    );
 }
 export default App;
